ba78dfbeefe9941f3af29ff066b5e4b122342937
DanielX <Daniel@Win10>
2020-07-17 16:32:06 -0600

    unicodestuff

--- source/win32/CVSTHost.cpp
+++ source/win32/CVSTHost.cpp
@@ -16,6 +16,8 @@
 
 #include "../CanDos.h"
 
+#include "unicodestuff.h"
+
 #define MAX_MIDI_EVENTS 4096 // far beyond what would ever normally appear in a single low-latency buffer (~256 samples or so)
 struct MyVSTEvents { // redeclaration of VstEvents, to support our own max number of events [see constant above]
     VstInt32 numEvents;
@@ -210,17 +212,6 @@ VstIntPtr VSTCALLBACK hostCallback(AEffect* effect, VstInt32 opcode, VstInt32 in
     }
     return 0;
 }
-
-//======================================================
-//   unicode util stuff, no need for a separate file
-#include <boost/locale/encoding_utf.hpp>
-
-using boost::locale::conv::utf_to_utf;
-
-std::wstring utf8_to_wstring(const std::string &str) {
-    return utf_to_utf<wchar_t>(str.c_str(), str.c_str() + str.size());
-}
-//======================================================
 
 CVSTHOST_API CVST_Plugin CDECL CVST_LoadPlugin(const char *pathToPlugin, void *userData)
 {
